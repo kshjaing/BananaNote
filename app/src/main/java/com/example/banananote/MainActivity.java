2078e8ce89669ab193477b8d502b0eab8c64578f
BaecMari <53397420+BaecMari@users.noreply.github.com>
2020-07-12 16:52:25 +0900

    테스트
    테스트

--- app/src/main/java/com/example/banananote/MainActivity.java
+++ app/src/main/java/com/example/banananote/MainActivity.java
@@ -32,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
+                //테스트입니다
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
