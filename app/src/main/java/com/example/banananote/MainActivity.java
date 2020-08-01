0325f5530c5c0758b3a06d922951bf406712af07
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-08-01 22:02:20 +0900

    .

--- app/src/main/java/com/example/banananote/MainActivity.java
+++ app/src/main/java/com/example/banananote/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //오른쪽 밑 플로팅 버튼
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
