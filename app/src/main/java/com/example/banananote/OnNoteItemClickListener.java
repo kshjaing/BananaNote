3776cd5ef94dc2a56f88c576c46a84a2620e9120
BaecMari <53397420+BaecMari@users.noreply.github.com>
2020-08-22 17:35:44 +0900

    .
    .

--- app/src/main/java/com/example/banananote/OnNoteItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.banananote;
-
-import android.view.View;
-
-public interface OnNoteItemClickListener {
-    //메모를 누를때 그 위치를 알아낸다.
-    public void onItemClick(NoteAdapter.ViewHolder holder, View view, int position);
-}
